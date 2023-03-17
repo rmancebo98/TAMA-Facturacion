be237bd2bb2bb300d389202fa7e766d293228b4a
robertomancebo <robertomancebo@outlook.com>
2023-03-16 21:53:48 -0400

    creation of notifications and initial design for the delete client window

--- src/main/java/clients.java
+++ src/main/java/clients.java
@@ -6,17 +6,20 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class clients {
 
     private static JFrame addFrame;
+    private static JFrame deleteFrame;
     private static final String clientsPath = "src/main/java/clients/clients.json";
 
     public static void createClientWindow() {
         addFrame = new JFrame("Agregar cliente");
         JPanel addPanel = new JPanel();
         JButton addClient;
+        JButton deleteClient;
 
         addPanel.add(new JLabel("Nombre del cliente :"));
         JTextField nameTxt = new JTextField(25);
@@ -32,12 +35,42 @@ public class clients {
             addValuesOnJson(nameTxt.getText(), rncTxt.getText());
         });
 
-        addPanel.add(addClient);
+        deleteClient = new JButton("Ir a eliminar cliente");
+        deleteClient.addActionListener(e -> showAddClientFrame());
 
+        addPanel.add(addClient);
+        addPanel.add(deleteClient);
         addFrame.add(addPanel);
         addFrame.setPreferredSize(new Dimension(300, 175));
         addFrame.pack();
         addFrame.setLocationRelativeTo(null);
+    }
+
+    public static void createDeleteClientWindow() {
+        deleteFrame = new JFrame("Eliminar cliente");
+        JPanel deletePanel = new JPanel();
+        JButton deleteClient;
+
+        deleteFrame.add(new JLabel("Clientes:"));
+        String[] options = clients.getAllKeysFromJson().toArray(new String[0]);
+        JComboBox<String> clientsDropDown = new JComboBox<>(options);
+
+        deleteClient = new JButton("Eliminar cliente");
+
+//        deleteClient.addActionListener(e -> addValuesOnJson(nameTxt.getText(), rncTxt.getText()));
+        //TODO: show the clients in a dropdown list and add an action listener to delete the delected one from clients.json
+        deletePanel.add(clientsDropDown);
+        deletePanel.add(deleteClient);
+
+        deleteFrame.add(deletePanel);
+        deleteFrame.setPreferredSize(new Dimension(300, 175));
+        deleteFrame.pack();
+        deleteFrame.setLocationRelativeTo(null);
+    }
+
+    public static void showAddClientFrame() {
+        createDeleteClientWindow();
+        deleteFrame.setVisible(true);
     }
 
     public static void showFrame() {
@@ -61,20 +94,39 @@ public class clients {
     public static void addValuesOnJson(String client, String RNC) {
         // Read the JSON file into a map
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, String> data = readClientsJson();
+        Map<String, String> clientData = readClientsJson();
+
+        client = client.toUpperCase();
 
         // Add the string to the map
-        data.put(client.toUpperCase(), RNC);
+        clientData.put(client, RNC);
 
         // Write the modified map back to the JSON file
         try {
-            mapper.writeValue(new File(clientsPath), data);
+            mapper.writeValue(new File(clientsPath), clientData);
             System.out.println("Data written to clients.json");
-            notifications.showPopUpNotification("Cliente añadido","Operación exitosa");
+
+            if (checkIfClientExist(client)) {
+                notifications.showPopUpNotification("El cliente ya existe, actualizando RNC", "Actualizando cliente");
+            } else {
+                notifications.showPopUpNotification("Cliente añadido", "Operación exitosa");
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
-            notifications.showPopUpNotification("Error añadiendo cliente","Hubo un error");
+            notifications.showErrorNotification("Error añadiendo cliente");
         }
+    }
+
+    public static void deleteValuesOnJson(String client) {
+        // Read the JSON file into a map
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> clientData = readClientsJson();
+
+        client = client.toUpperCase();
+    }
+
+    public static boolean checkIfClientExist(String client) {
+        return readClientsJson().get(client) != null && !Objects.equals(readClientsJson().get(client), "");
     }
 
     public static Set<String> getAllKeysFromJson() {
--- src/main/java/notifications.java
+++ src/main/java/notifications.java
@@ -3,6 +3,11 @@ import javax.swing.*;
 public class notifications {
 
     public static void showPopUpNotification(String message, String title){
-        JOptionPane.showMessageDialog(null, message, title, JOptionPane.WARNING_MESSAGE);
+        ImageIcon icon = new ImageIcon("src/main/resources/icons/done.png");
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.WARNING_MESSAGE, icon);
+    }
+
+    public static void showErrorNotification(String message){
+        JOptionPane.showMessageDialog(null, message, "Ocurrió un error", JOptionPane.ERROR_MESSAGE);
     }
 }
